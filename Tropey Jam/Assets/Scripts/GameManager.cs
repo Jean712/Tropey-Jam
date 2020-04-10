49f74fbd077615efbb7e6fd0eca3e16734a385bc
Jean Harang <J.Harang@student.isartdigital.com>
2020-04-10 21:08:47 +0200

    Initialization
    Basics

---
+++ Tropey Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
