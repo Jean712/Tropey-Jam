523e848359423737e4a60ea796c4756c585a9fbc
Barreyre <barreyre26@gmail.com>
2020-04-10 23:05:01 +0200

    +SpawnManager
    J'ai ajouté le spawnmanager

---
+++ Assets/Scripts/SpawnManaher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManaher : MonoBehaviour
+{
+    [Header("Spawn Des Tresors")]
+    [SerializeField]
+    private int TreasuresToSpawn;
+    [SerializeField]
+    private int TreasuresSpawned;
+    private Transform[] SpawnPositions;
+    [SerializeField]
+    private GameObject[] Treasures;
+
+
+
+    void Start()
+    {
+        for (TreasuresSpawned = 0; TreasuresSpawned < TreasuresToSpawn; TreasuresSpawned++)
+        {
+            //Définir pos random
+            //Instantiate Trésor sur la position
+            //Suppr position
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
