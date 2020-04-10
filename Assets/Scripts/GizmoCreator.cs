23c421b9230aa3e03f78fd1d642e07154f6e1b0e
Jean Harang <J.Harang@student.isartdigital.com>
2020-04-10 22:43:22 +0200

    Gros commit
    tu peux être gros si on t'enlève 3809 fichiers ? :thinking:

---
+++ Assets/Scripts/GizmoCreator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoCreator : MonoBehaviour
+{
+    private enum gizmoAspect {Sphere, WireSphere, Cube, WireCube, Ray};
+    
+    [SerializeField]
+    private gizmoAspect shape;
+
+    [SerializeField]
+    [Range(0.1f, 10)]
+    private float gizmoSize = 0.25f;
+    [SerializeField]
+    private Color gizmoColor = Color.green;
+
+    private Vector3 cubeSize;
+
+    private void OnDrawGizmos()
+    {
+        cubeSize = new Vector3(gizmoSize, gizmoSize, gizmoSize) * 1.5f;
+
+        Gizmos.color = gizmoColor;
+        switch (shape)
+        {
+            case gizmoAspect.Sphere:
+                Gizmos.DrawSphere(transform.position, gizmoSize);
+                break;
+
+            case gizmoAspect.WireSphere:
+                Gizmos.DrawWireSphere(transform.position, gizmoSize);
+                break;
+
+            case gizmoAspect.Cube:
+                Gizmos.DrawCube(transform.position, cubeSize);
+                break;
+
+            case gizmoAspect.WireCube:
+                Gizmos.DrawWireCube(transform.position, cubeSize);
+                break;
+
+            case gizmoAspect.Ray:
+                Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * gizmoSize);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ProtoPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoPlayer : MonoBehaviour
+{
+    private Rigidbody rgbd;
+    private Queue inventory;
+
+    public float speed;
+    public float maxSpeed;
+
+    private void Awake()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rgbd.velocity = Vector3.Lerp(rgbd.velocity, Vector3.zero, 0.05f);
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rgbd.velocity += new Vector3(0, 0, 1 * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rgbd.velocity += new Vector3(0, 0, -1 * speed);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rgbd.velocity += new Vector3(-1 * speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rgbd.velocity += new Vector3(1 * speed, 0, 0);
+        }
+
+        rgbd.velocity = Vector3.ClampMagnitude(rgbd.velocity, maxSpeed);
+
+
+    }
+}
