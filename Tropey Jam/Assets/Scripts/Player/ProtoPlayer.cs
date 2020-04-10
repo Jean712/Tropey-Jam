78b7b935b3452efc89f2c670e909282fe5ac24b3
Jean Harang <J.Harang@student.isartdigital.com>
2020-04-10 22:19:19 +0200

    Les pizzas
    2 Reines et 2 Pepperonis

---
+++ Tropey Jam/Assets/Scripts/Player/ProtoPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoPlayer : MonoBehaviour
+{
+    private Rigidbody rgbd;
+    public float speed;
+
+    private void Awake()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rgbd.velocity += new Vector3(0, 0, 1 * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rgbd.velocity += new Vector3(0, 0, -1 * speed);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rgbd.velocity += new Vector3(-1 * speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rgbd.velocity += new Vector3(1 * speed, 0, 0);
+        }
+    }
+}
