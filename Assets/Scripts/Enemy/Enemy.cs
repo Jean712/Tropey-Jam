8ba6600ba38da5ce2571669771c403bdab963e3e
Andoni <a.iturioz@student.isartdigital.com>
2020-04-11 10:50:58 -0400

    Game Over marche bien

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -139,4 +139,12 @@ public class Enemy : MonoBehaviour
 
         return false;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            manager.GameOver();
+        }
+    }
 }
