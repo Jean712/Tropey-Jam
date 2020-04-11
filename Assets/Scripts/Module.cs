dcfa17164f23684213e34939fff075672016495f
Andoni <a.iturioz@student.isartdigital.com>
2020-04-11 16:26:15 -0400

    les modules tournent

---
+++ Assets/Scripts/Module.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module : MonoBehaviour
+{   
+    void Start()
+    {
+        transform.Rotate(Vector3.up, 90 * Random.Range(0, 4));
+    }
+}
