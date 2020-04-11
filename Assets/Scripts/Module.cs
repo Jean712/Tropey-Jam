ec2786484750430e22731b7a5155a8ef5a5cf055
Andoni <a.iturioz@student.isartdigital.com>
2020-04-11 18:09:18 -0400

    la route aux enfers

--- Assets/Scripts/Module.cs
+++ Assets/Scripts/Module.cs
@@ -3,9 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Module : MonoBehaviour
-{   
+{
+    [Header("Spawn Des Tresors")]
+    private int TreasuresSpawned;
+
+    private float TreasuresToSpawn;
+
+    [SerializeField]
+    private List<Transform> SpawnPositions;
+
+    [SerializeField]
+    private List<GameObject> Treasures;
+
     void Start()
     {
         transform.Rotate(Vector3.up, 90 * Random.Range(0, 4));
+        TreasuresToSpawn = Treasures.Count;
+        InstantiateTreasures();
+    }
+    private void InstantiateTreasures()
+    {
+        for (TreasuresSpawned = 0; TreasuresSpawned < TreasuresToSpawn; TreasuresSpawned++)
+            InstantiateTreasure();
+    }
+    private void InstantiateTreasure()
+    {
+        int SpawnTransformIndex = Random.Range(0, SpawnPositions.Count);
+        Transform SpawnTransform = SpawnPositions[SpawnTransformIndex];
+
+        int TreasureToSpawnIndex = Random.Range(0, Treasures.Count);
+        GameObject TreasureToSpawn = Treasures[TreasureToSpawnIndex];
+
+        GameObject SpawnedObject = Instantiate(TreasureToSpawn, SpawnTransform.position, SpawnTransform.rotation);
+
+        Treasures.RemoveAt(TreasureToSpawnIndex);
+        SpawnPositions.RemoveAt(SpawnTransformIndex);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,16 +3,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [Header("Spawn Des Tresors")]
-    private int TreasuresSpawned;
     
-    private float TreasuresToSpawn;
-
-    [SerializeField]
-    private List<Transform> SpawnPositions;
-
-    [SerializeField]
-    private List<GameObject> Treasures;
 
     [Header("Modules")]
     public GameObject[] moduleList;
@@ -30,29 +21,9 @@ public class SpawnManager : MonoBehaviour
         {
             AddModule();
         }
-        TreasuresToSpawn = Treasures.Count;
-        InstantiateTreasures();
+        
     }
-
-    private void InstantiateTreasures()
-    {
-        for (TreasuresSpawned = 0; TreasuresSpawned < TreasuresToSpawn; TreasuresSpawned++)
-            InstantiateTreasure();
-    }
-
-    private void InstantiateTreasure()
-    {
-        int SpawnTransformIndex = Random.Range(0, SpawnPositions.Count);
-        Transform SpawnTransform = SpawnPositions[SpawnTransformIndex];
-
-        int TreasureToSpawnIndex = Random.Range(0, Treasures.Count);
-        GameObject TreasureToSpawn = Treasures[TreasureToSpawnIndex];
-
-        GameObject SpawnedObject = Instantiate(TreasureToSpawn, SpawnTransform.position, SpawnTransform.rotation);
- 
-        Treasures.RemoveAt(TreasureToSpawnIndex);
-        SpawnPositions.RemoveAt(SpawnTransformIndex);
-    }
+    
 
     void AddModule()
     {
