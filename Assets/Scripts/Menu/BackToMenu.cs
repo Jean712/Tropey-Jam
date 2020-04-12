19f435548345a453654f7ce9dbc186f4c79d50eb
Barreyre <barreyre26@gmail.com>
2020-04-12 17:08:51 +0200

    TOUS ENSEMBLEUUUUUH

---
+++ Assets/Scripts/Menu/BackToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    [SerializeField]
+    private float CreditsTimer;
+
+    private void Update()
+    {
+        CreditsTimer -= Time.deltaTime;
+
+        if (CreditsTimer <= 0 || Input.anyKeyDown)
+            SceneManager.LoadScene("MainMenu");
+    }
+}
